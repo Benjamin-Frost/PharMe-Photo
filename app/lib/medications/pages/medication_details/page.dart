c33f28ea8c5dfcb2ec1ed9bcaf3b3965c5c57c7f
DevSchmidtchen <contact@schmidtchen.me>
2022-02-15 12:03:33 +0100

    display clinical annotations in app

--- app/lib/medications/pages/medication_details/page.dart
+++ app/lib/medications/pages/medication_details/page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:app/medications/module.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -25,8 +26,20 @@ Future<Medication> fetchMedication(String setId) async {
   }
 }
 
+class ClinicalAnnotation {
+  ClinicalAnnotation({
+    required this.gene,
+    required this.allele,
+    required this.phenotypes,
+  });
+
+  final String gene;
+  final String allele;
+  final List<String> phenotypes;
+}
+
 class Medication {
-  const Medication({
+  Medication({
     required this.name,
     required this.manufacturer,
     required this.agents,
@@ -42,29 +55,33 @@ class Medication {
           .toList(),
     );
 
-    medication.fetchClinicalAnnotations();
+    medication.futureClinicalAnnotations =
+        medication.fetchClinicalAnnotations();
 
     return medication;
   }
 
-  Future<void> fetchClinicalAnnotations() async {
+  Future<List<ClinicalAnnotation>> fetchClinicalAnnotations() async {
     final responses = await Future.wait(agents.map((agent) =>
         http.get(Uri.https('api.pharmgkb.org', '/v1/data/clinicalAnnotation', {
           'relatedChemicals.name': agent.toLowerCase(),
         }))));
 
+    final annotations = <ClinicalAnnotation>[];
+
     for (final response in responses) {
       final body = jsonDecode(response.body);
-      final annotations = (body['data'] as List<dynamic>).where((annotation) {
+      final jsonAnnotations =
+          (body['data'] as List<dynamic>).where((annotation) {
         final genes = annotation['location']['genes'] as List<dynamic>;
         final relatedGenes = genes.map((gene) => gene['symbol'] as String);
 
         return relatedGenes.contains('CYP2C9');
       });
 
-      print(annotations.toString());
+      final phenotypes = <String>[];
 
-      for (final annotation in annotations) {
+      for (final annotation in jsonAnnotations) {
         final allelePhenotype = annotation['allelePhenotypes'].firstWhere(
           (allelePhenotype) => allelePhenotype['allele'] == '*1',
         );
@@ -73,14 +90,20 @@ class Medication {
           continue;
         }
 
-        print(allelePhenotype['phenotype']);
+        phenotypes.add(allelePhenotype['phenotype']);
       }
+
+      annotations.add(ClinicalAnnotation(
+          gene: 'CYP2C9', allele: '*1', phenotypes: phenotypes));
     }
+
+    return annotations;
   }
 
   final String name;
   final String manufacturer;
   final List<String> agents;
+  late Future<List<ClinicalAnnotation>> futureClinicalAnnotations;
 }
 
 class _MedicationDetailsPageState extends State<MedicationDetailsPage> {
@@ -95,27 +118,49 @@ class _MedicationDetailsPageState extends State<MedicationDetailsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FutureBuilder<Medication>(
-        future: futureMedication,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(snapshot.data!.name),
-                Text(snapshot.data!.manufacturer),
-                Text(snapshot.data!.agents.toString()),
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return Text('${snapshot.error}');
-          }
+    return FutureBuilder<Medication>(
+      future: futureMedication,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(snapshot.data!.name),
+              Text(snapshot.data!.manufacturer),
+              Text(snapshot.data!.agents.toString()),
+              FutureBuilder<List<ClinicalAnnotation>>(
+                  future: snapshot.data!.futureClinicalAnnotations,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        children: [
+                          for (final annotation in snapshot.data!)
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Gene: ${annotation.gene}'),
+                                Text('Your genotype: ${annotation.allele}'),
+                                for (final phenotype in annotation.phenotypes)
+                                  Text(phenotype),
+                              ],
+                            )
+                        ],
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
 
-          // By default, show a loading spinner.
-          return const CircularProgressIndicator();
-        },
-      ),
+                    return const CircularProgressIndicator();
+                  }),
+            ],
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        // By default, show a loading spinner.
+        return Center(child: const CircularProgressIndicator());
+      },
     );
   }
 }
