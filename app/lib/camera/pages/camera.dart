755b1fbdc0b48c891fd3df4fb2e4547037138123
Benjamin Frost <bfrost.schule@gmail.com>
2022-05-23 22:18:54 +0200

    feat(app): create camera preview

--- app/lib/camera/pages/camera.dart
+++ app/lib/camera/pages/camera.dart
@@ -5,6 +5,64 @@ class CameraPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Hello World');
+    return FutureBuilder<List<CameraDescription>>(
+      future: availableCameras(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          final cameras = snapshot.data;
+          if (cameras == null || cameras.isEmpty) {
+            return Center(
+              child: Text('No cameras found'),
+            );
+          } else {
+            return CameraPreviewWrapper(cameras.first);
+          }
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
+
+class CameraPreviewWrapper extends StatefulWidget {
+  const CameraPreviewWrapper(
+    this.camera, {
+    Key? key,
+  }) : super(key: key);
+
+  final CameraDescription camera;
+
+  @override
+  State<CameraPreviewWrapper> createState() => _CameraPreviewWrapperState();
+}
+
+class _CameraPreviewWrapperState extends State<CameraPreviewWrapper> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(widget.camera, ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) return;
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Center(child: CircularProgressIndicator());
+    }
+    return MaterialApp(
+      home: CameraPreview(controller),
+    );
   }
 }
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,6 +3,7 @@ import 'common/module.dart';
 Future<void> main() async {
   // await initServices();
   // await fetchAndSaveLookups();
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(PharmeApp());
   // await cleanupServices();
 }
